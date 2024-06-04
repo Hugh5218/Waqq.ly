a438fd91ee3219ea6e4d98e2bc4e74bd47ade121
Hugh Davison <H.Davison.21@unimail.winchester.ac.uk>
2024-06-04 22:04:36 +0100

    Change16

--- backend/getPets/index.js
+++ backend/getPets/index.js
@@ -3,6 +3,21 @@ const { MongoClient } = require('mongodb');
 module.exports = async function (context, req) {
     context.log('Function getPets is starting.');
 
+    context.res = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'https://agreeable-pebble-031a20b03.5.azurestaticapps.net',
+            'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+        }
+    };
+
+    if (req.method === 'OPTIONS') {
+        context.res.status = 204;
+        context.done();
+        return;
+    }
+
     const uri = process.env.MONGO_DB_CONNECTION_STRING;
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -14,11 +29,13 @@ module.exports = async function (context, req) {
         const petsList = await pets.find({}).toArray();
 
         context.res = {
+            ...context.res,
             status: 200,
             body: petsList
         };
     } catch (error) {
         context.res = {
+            ...context.res,
             status: 500,
             body: `Internal Server Error: ${error.message}`
         };
