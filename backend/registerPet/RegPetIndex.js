880b27708fb64a02edda611c56f81e46a4a697f0
Hugh Davison <H.Davison.21@unimail.winchester.ac.uk>
2024-06-04 23:22:01 +0100

    Change20

--- backend/registerPet/RegPetIndex.js
+++ backend/registerPet/RegPetIndex.js
@@ -3,6 +3,21 @@ const { MongoClient } = require('mongodb');
 module.exports = async function (context, req) {
     context.log('Function registerPet is starting.');
 
+    context.res = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'https://agreeable-pebble-031a20b03.5.azurestaticapps.net',
+            'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+        }
+    };
+
+    if (req.method === 'OPTIONS') {
+        context.res.status = 204;
+        context.done();
+        return;
+    }
+
     const uri = process.env.MONGO_DB_CONNECTION_STRING;
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
